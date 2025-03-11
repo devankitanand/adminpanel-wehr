424812ad006294ea8734b2d4bce6e768ddb91604
Ankit Anand <devankitanand@gmail.com>
2025-03-11 06:15:31 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,5 @@ import withPWA from 'next-pwa';
 console.log('Configuring image domains:', ['localhost', 'api-digilabs.onrender.com']);
 
 export default withPWA({
-  dest: 'public',
-  images: {
-    domains: ['localhost', 'api-digilabs.onrender.com'],
-  },
+  dest: 'public'
 });
